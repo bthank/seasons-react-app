bd33f1fd81935c544815584f1645cf88c2fddf0e
bthank <bthankachan@yahoo.com>
2021-09-12 22:25:14 -0400

    Styling changes

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,4 +1,17 @@
+import './SeasonDisplay.css';
 import React from 'react';
+
+// use this type of pattern of creating a config object
+const seasonConfig = {
+    summer: {
+        text: "Let's hit the beach!",
+        iconName: 'sun',
+    },
+    winter: {
+        text: 'Burr, it is cold!',
+        iconName: 'snowflake',
+    }
+};
 
 // this getSeason function will take the current latitude and month and return the season
 const getSeason = (lat,month) => {
@@ -7,10 +20,10 @@ const getSeason = (lat,month) => {
     if (month > 2 && month < 9){
         // use JavaScript ternary expression; 
         // if lat > 0 then we are in Northern Hemisphere
-        return lat > 0 ? 'Summer' : 'Winter';
+        return lat > 0 ? 'summer' : 'winter';
     }
     else {
-        return lat > 0 ? 'Winter' : 'Summer';
+        return lat > 0 ? 'winter' : 'summer';
     }; 
  
 };
@@ -22,19 +35,22 @@ const SeasonDisplay = (props) => {
 
     const season = getSeason(props.lat, new Date().getMonth());
     console.log(season);   
+
+    // destructure out text and iconName from seasonConfig object
+    const {text,iconName} = seasonConfig[season];
     
-    const text = season === 'Winter' ? 'Burr, it is chilly!' : 'Lets hit the beach!';
-    const icon = season === 'Winter' ? 'snowflake' : 'sun';
+   // const text = season === 'Winter' ? 'Burr, it is chilly!' : 'Lets hit the beach!';
+   // const icon = season === 'Winter' ? 'snowflake' : 'sun';
 
     //return <div>SeasonDisplay</div>;
     // this is JSX interpolation
     return (
             // for icon className, display name of the icon followed by word icon in backticks
             // this is required for semantic-ui notation
-            <div>
-                <i className={`${icon} icon`} />
+            <div className={`season-display ${season}`}>
+                <i className={`icon-left massive ${iconName} icon`} />
                 <h1> { text } </h1>
-                <i className={`${icon} icon`} />
+                <i className={`icon-right massive ${iconName} icon`} />
             </div>
             );
 };
